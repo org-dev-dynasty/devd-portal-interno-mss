c6829c41a8f1dd6c37341ec79651f1c7e02ea252
jotaapee <galhardojoaopedro@gmail.com>
2024-07-02 01:41:17 -0300

    feat: Add project repository interface

---
+++ src/shared/domain/repositories/project_repository_interface.ts
@@ -0,0 +1,10 @@
+import { Project, ProjectProps } from "../entities/project";
+
+
+export interface IProjectRepository { 
+    createProject(projectProps: ProjectProps): Promise<Project>;
+    // updateProject(project: Project): Promise<Project>;
+    // deleteProject(projectId: string): Promise<void>;
+    // getProjectById(projectId: string): Promise<Project | undefined>;
+    // getAllProjects(): Promise<Project[]>;
+}  
