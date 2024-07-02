4f1ebb60280c22cf40a4acbb7d36cf267ba2b673
jotaapee <galhardojoaopedro@gmail.com>
2024-07-02 17:21:39 -0300

    feat: Add GetAllProjectPresenter for handling retrieval of all projects

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Express, Request, Response } from "express";
 import CreateUserPresenter from "../modules/create_user/app/create_user_presenter";
 import AuthUserPresenter from "../modules/auth_user/app/auth_user_presenter";
 import CreateProjectPresenter from "../modules/create_project/app/create_project_presenter";
+import GetAllProjectPresenter from "../modules/get_all_projects/app/get_all_projects_presenter";
 
 import app from "../app";
 
@@ -18,6 +19,7 @@ const routes = (app: Express) => {
 
   // project routes
   app.use("/api", CreateProjectPresenter);
+  app.use("/api", GetAllProjectPresenter);
 };
 
 export default routes;
