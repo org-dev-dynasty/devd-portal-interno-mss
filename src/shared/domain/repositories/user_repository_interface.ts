0b36fb8c04546cb3aa2a3266b5bf665a1a7cc1ca
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-02 01:13:04 -0300

    refactor: Add getUserById method to UserRepositoryInterface

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -3,4 +3,5 @@ import { User } from "../entities/user";
 export interface IUserRepository {
   createUser(user: User): Promise<User>;
   getUserByEmail(email: string): Promise<User | undefined>
+  getUserById(id: string): Promise<User | undefined>
 }
