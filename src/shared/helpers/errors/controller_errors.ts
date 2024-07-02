39db380a7760d9a9dae1985c773460930a70193f
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-01 22:04:04 -0300

    chore: Add InvalidRequest and InvalidParameter error classes

--- src/shared/helpers/errors/controller_errors.ts
+++ src/shared/helpers/errors/controller_errors.ts
@@ -1,5 +1,21 @@
 import { BaseError } from "./base_error";
 
+export class InvalidRequest extends BaseError {
+  constructor(parameter?: string) {
+      if (parameter) {
+          super(parameter + " not found");
+      } else {
+          super("No request found");
+      }
+  }
+}
+
+export class InvalidParameter extends BaseError {
+  constructor(parameter: string, value: string) {
+      super(`Invalid parameter: ${parameter}: ${value}`);
+  }
+}
+
 export class MissingParameters extends BaseError {
   constructor(message: string) {
     super(`Field ${message} is missing`);
