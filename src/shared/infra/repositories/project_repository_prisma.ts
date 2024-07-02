885fb7c8d0f6da7ff5defb0e0e92840721ab2c93
jotaapee <galhardojoaopedro@gmail.com>
2024-07-02 17:21:59 -0300

    feat: Update project repository to include getAllProjects method

--- src/shared/infra/repositories/project_repository_prisma.ts
+++ src/shared/infra/repositories/project_repository_prisma.ts
@@ -6,23 +6,22 @@ import { toEnum } from "../../domain/enums/status_enum";
 const prisma = new PrismaClient();
 
 export class ProjectRepositoryPrisma implements IProjectRepository {
-
     async createProject(projectProps: ProjectProps): Promise<Project> {
         try {
             const createdProjectFromPrisma = await prisma.project.create({
                 data: {
-                    name: projectProps.projectName,
-                    status: projectProps.projectStatus,
-                    description: projectProps.projectDescription || '',
+                    projectName: projectProps.projectName,
+                    projectStatus: projectProps.projectStatus,
+                    projectDescription: projectProps.projectDescription || '',
                 },
             });
-    
+
             const createdProject = new Project({
-                projectName: createdProjectFromPrisma.name,
-                projectStatus: toEnum(createdProjectFromPrisma.status),
-                projectDescription: createdProjectFromPrisma.description,
+                projectName: createdProjectFromPrisma.projectName,
+                projectStatus: toEnum(createdProjectFromPrisma.projectStatus),
+                projectDescription: createdProjectFromPrisma.projectDescription,
             });
-    
+
             return createdProject;
         } catch (error: any) {
             console.error("Erro ao criar projeto:", error);
@@ -30,6 +29,27 @@ export class ProjectRepositoryPrisma implements IProjectRepository {
                 throw new Error("Projeto já cadastrado.");
             }
             throw new Error("Erro ao criar projeto no banco de dados.");
+        }
+    }
+
+    async getAllProjects(): Promise<Project[]> {
+        try {
+            const projectsFromPrisma = await prisma.project.findMany();
+
+            const projects = projectsFromPrisma.map((project) => {
+                return new Project({
+                    projectName: project.projectName,
+                    projectStatus: toEnum(project.projectStatus),
+                    projectDescription: project.projectDescription,
+                });
+            });
+
+            console.log("Projetos encontrados:", projects);
+
+            return projects;
+        } catch (error: any) {
+            console.log("Erro ao buscar projetos:", error);
+            throw new Error("Erro ao buscar projetos no banco de dados.");
         }
     }
 }
