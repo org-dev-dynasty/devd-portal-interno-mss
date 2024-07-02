8dc9f6cfaed7830d8c5d82b9d0aadb146078a26c
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-02 02:48:49 -0300

    chore: Add JWT authentication middleware to create-project route

--- src/modules/create_project/app/create_project_presenter.ts
+++ src/modules/create_project/app/create_project_presenter.ts
@@ -3,13 +3,14 @@ import express, { Request, Response } from "express";
 import { ProjectRepositoryPrisma } from "../../../shared/infra/repositories/project_repository_prisma";
 import { CreateProjectUsecase } from "./create_project_usecase";
 import { CreateProjectController } from "./create_project_controller";
+import { authenticateToken } from "../../../shared/middlewares/jwt_middleware";
 
 const router = express.Router();
 const projectRepository = new ProjectRepositoryPrisma();
 const projectUseCase = new CreateProjectUsecase(projectRepository);
 const projectController = new CreateProjectController(projectUseCase);
 
-router.post("/create-project", async (req: Request, res: Response) => {
+router.post("/create-project", authenticateToken, async (req: Request, res: Response) => {
     await projectController.createProject(req, res);
   });
   
