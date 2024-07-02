297ea604059b0507cf804f2b5415774bf0cbcf11
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-02 03:15:45 -0300

    chore: Update server.ts to use port 3020 instead of 3000

--- server.ts
+++ server.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import app from "./src/app";
 import routes from "./src/routes/routes";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3020;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT} 🚀`);
 });
 
 import cors from "cors";
