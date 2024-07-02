5c3ef14da4f79908c291d1745a55e47c4c486caf
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-02 14:48:12 -0300

    refactor: Update import paths for getUserByIdUsecase

--- src/modules/get_user_by_id/app/get_user_by_id_usecase.ts
+++ src/modules/get_user_by_id/app/get_user_by_id_usecase.ts
@@ -1,6 +1,6 @@
-import { User } from "../../../../shared/domain/entities/user";
-import { IUserRepository } from "../../../../shared/domain/repositories/user_repository_interface";
-import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
+import { User } from "../../../shared/domain/entities/user";
+import { IUserRepository } from "../../../shared/domain/repositories/user_repository_interface";
+import { NoItemsFound } from "../../../shared/helpers/errors/usecase_errors";
 
 export class getUserByIdUsecase {
   constructor(private readonly userRepository: IUserRepository) {}
