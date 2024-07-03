894c794138a7ec9fff74156e7dcf01702e453fbc
jotaapee <galhardojoaopedro@gmail.com>
2024-07-03 18:08:46 -0300

    feat: Update updateProject method in ProjectRepositoryInterface

--- src/shared/domain/repositories/project_repository_interface.ts
+++ src/shared/domain/repositories/project_repository_interface.ts
@@ -1,9 +1,10 @@
 import { Project, ProjectProps } from "../entities/project";
+import { STATUS } from "../enums/status_enum";
 
 
 export interface IProjectRepository { 
     createProject(projectProps: Project): Promise<Project>;
-    // updateProject(project: Project): Promise<Project>;
+    updateProject(projectId: string, projectName?: string, projectDescription?: string, projectStatus?: STATUS): Promise<Project>;
     // deleteProject(projectId: string): Promise<void>;
     getProjectById(projectId: string): Promise<Project | undefined>;
     getAllProjects(): Promise<Project[]>;
