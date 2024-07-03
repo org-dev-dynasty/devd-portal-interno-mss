41e11e839917a793ae769a74858691f530725c09
jotaapee <galhardojoaopedro@gmail.com>
2024-07-03 01:43:04 -0300

    feat: Implement getProjectById method in ProjectRepositoryInterface

--- src/shared/domain/repositories/project_repository_interface.ts
+++ src/shared/domain/repositories/project_repository_interface.ts
@@ -5,6 +5,6 @@ export interface IProjectRepository {
     createProject(projectProps: ProjectProps): Promise<Project>;
     // updateProject(project: Project): Promise<Project>;
     // deleteProject(projectId: string): Promise<void>;
-    // getProjectById(projectId: string): Promise<Project | undefined>;
+    getProjectById(projectId: string): Promise<Project | undefined>;
     // getAllProjects(): Promise<Project[]>;
 }  
