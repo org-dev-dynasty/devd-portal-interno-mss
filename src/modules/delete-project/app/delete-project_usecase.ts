5bacaa7459b52b204393f52cde0b7af42d28de10
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-03 23:28:20 -0300

    refactor: Simplify DeleteProjectUsecase implementation

--- src/modules/delete-project/app/delete-project_usecase.ts
+++ src/modules/delete-project/app/delete-project_usecase.ts
@@ -1,24 +1,10 @@
 import { IProjectRepository } from "../../../shared/domain/repositories/project_repository_interface";
-import { EntityError } from "../../../shared/helpers/errors/domain_errors";
-
 
 export class DeleteProjectUsecase {
-    constructor(private repo: IProjectRepository) {
-        this.repo = repo;
-    }
-
-    async execute(projectId: string) {
-        try {
-            const project = await this.repo.deleteProject(projectId);
-            if (project === undefined) {
-                throw new Error("Project not found.");
-            }
-            console.log("Projeto deletado:", project);
-            return project;
-        }
-        catch (error: any) {
-            console.error("Erro ao deletar projeto:", error);
-            throw new EntityError("Project");
-        }
-    }
+  constructor(private repo: IProjectRepository) {
+    this.repo = repo;
+  }
+  async execute(projectId: string): Promise<void> {
+    await this.repo.deleteProject(projectId);
+  }
 }
