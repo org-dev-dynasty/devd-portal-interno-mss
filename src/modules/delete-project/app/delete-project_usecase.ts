d650ba70ad8dfaa60f5e736900edf1ac87c48801
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-03 23:35:21 -0300

    refactor: Improve error handling in DeleteProjectUsecase

--- src/modules/delete-project/app/delete-project_usecase.ts
+++ src/modules/delete-project/app/delete-project_usecase.ts
@@ -1,10 +1,14 @@
 import { IProjectRepository } from "../../../shared/domain/repositories/project_repository_interface";
+import { NotFound } from "../../../shared/helpers/http/http_codes";
 
 export class DeleteProjectUsecase {
-  constructor(private repo: IProjectRepository) {
-    this.repo = repo;
-  }
+  constructor(private repo: IProjectRepository) {}
+
   async execute(projectId: string): Promise<void> {
-    await this.repo.deleteProject(projectId);
+    try {
+      await this.repo.deleteProject(projectId);
+    } catch (error) {
+      throw new NotFound("Projeto não encontrado");
+    }
   }
 }
