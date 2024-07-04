b3caf1bf156e75d1cccabe2ba1ee550fb4094109
jotaapee <galhardojoaopedro@gmail.com>
2024-07-03 22:49:08 -0300

    refactor: Add DeleteProjectController for handling project deletion

---
+++ src/modules/delete-project/app/delete-project_controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { DeleteProjectUsecase } from "./delete-project_usecase";
+import { DeleteProjectViewModel } from "./delete-project_viewmodel";
+import { BadRequest, Forbidden, InternalServerError, ParameterError } from "../../../shared/helpers/http/http_codes";
+import { InvalidParameter, InvalidRequest, MissingParameters } from "../../../shared/helpers/errors/controller_errors";
+import { EntityError } from "../../../shared/helpers/errors/domain_errors";
+import { NoItemsFound } from "../../../shared/helpers/errors/usecase_errors";
+
+export class DeleteProjectController {
+   constructor(private usecase: DeleteProjectUsecase) {}
+
+   async handle(req: Request, res: Response) {
+    try {
+        const projectId: string = req.params.projectId;
+
+        if (!req.params) {
+          throw new MissingParameters("Missing parameters.")
+        }
+
+        if (!projectId) { 
+          throw new MissingParameters("Missing project id.");
+        }
+
+        const project = await this.usecase.execute(projectId);
+        const viewModel = new DeleteProjectViewModel(project);
+        return res.status(200).json(viewModel);
+    } catch (error: any) {
+        if (error instanceof InvalidRequest) {
+          return new BadRequest(error.message).send(res);
+        }
+        if (error instanceof InvalidParameter) {
+          return new ParameterError(error.message).send(res);
+        }
+        if (error instanceof EntityError) {
+          return new ParameterError(error.message).send(res);
+        }
+        if (error instanceof Forbidden) {
+          return new Forbidden(error.getMessage()).send(res);
+        }
+        if (error instanceof NoItemsFound) {
+          return new Forbidden(error.message).send(res);
+        }
+        return new InternalServerError('Internal Server Error').send(res);
+      }
+   }
+}
