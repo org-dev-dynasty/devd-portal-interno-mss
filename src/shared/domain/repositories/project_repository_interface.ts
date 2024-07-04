29ace445d28e3d644efbbbba5a98c81c76e2c222
jotaapee <galhardojoaopedro@gmail.com>
2024-07-03 22:50:35 -0300

    refactor: Enable project deletion functionality
    The commit enables the project deletion functionality by adding the `deleteProject` method to the `IProjectRepository` interface. This method allows for the deletion of a project by its ID. The addition of this method improves the codebase by providing a dedicated mechanism for project deletion.
    
    Refs: #14

--- src/shared/domain/repositories/project_repository_interface.ts
+++ src/shared/domain/repositories/project_repository_interface.ts
@@ -5,7 +5,7 @@ import { STATUS } from "../enums/status_enum";
 export interface IProjectRepository { 
     createProject(projectProps: Project): Promise<Project>;
     updateProject(projectId: string, projectName?: string, projectDescription?: string, projectStatus?: STATUS): Promise<Project>;
-    // deleteProject(projectId: string): Promise<void>;
+    deleteProject(projectId: string): Promise<void>;
     getProjectById(projectId: string): Promise<Project | undefined>;
     getAllProjects(): Promise<Project[]>;
 }  
