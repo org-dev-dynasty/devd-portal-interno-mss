643c4bf482cad4aa3f7ef4f68d8a1c30a46105df
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-05 19:55:17 -0300

    refactor: Update getUserByEmail method return type to use IUserAll interface

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -1,8 +1,8 @@
-import { User } from "../entities/user";
+import { IUserAll, User } from "../entities/user";
 
 export interface IUserRepository {
   createUser(user: User): Promise<User>;
-  getUserByEmail(email: string): Promise<User | undefined>;
+  getUserByEmail(email: string): Promise<IUserAll | undefined>;
   getUserById(id: string): Promise<User | undefined>;
   getAllUsers(): Promise<User[]>;
   updateUserStatus(id: string, status: string): Promise<boolean>;
