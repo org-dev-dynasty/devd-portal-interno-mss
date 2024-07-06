39f317e8c5dcbb57ce31fafd0f9b357ce3b0d685
YuriDrapack <drapackyuri@gmail.com>
2024-07-06 17:57:58 -0300

    refactor: Update routes.ts to include GetTaskByIdPresenter

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,15 +5,15 @@ import AuthUserPresenter from "../modules/auth_user/app/auth_user_presenter";
 import CreateProjectPresenter from "../modules/create_project/app/create_project_presenter";
 import CreateTaskPresenter from "../modules/create_task/app/create_task_presenter";
 import CreateUserPresenter from "../modules/create_user/app/create_user_presenter";
-import GetAllUsersPresenter from "../modules/get_all_user/app/get_all_users_presenter";
-import GetUserByIdPresenter from "../modules/get_user_by_id/app/get_user_by_id_presenter";
-import UpdateUserStatusPresenter from "../modules/update_user_status/app/update_user_status_presenter";
-import GetAllProjectPresenter from "../modules/get_all_projects/app/get_all_projects_presenter";
-import GetProjectByIdPresenter from "../modules/get_project_by_id/app/get_project_by_id_presenter";
-import UpdateProjectPresenter from "../modules/update_project_by_id/app/update_project_presenter";
 import DeleteProjectPresenter from "../modules/delete-project/app/delete-project_presenter";
+import GetAllProjectPresenter from "../modules/get_all_projects/app/get_all_projects_presenter";
+import GetAllUsersPresenter from "../modules/get_all_user/app/get_all_users_presenter";
+import GetProjectByIdPresenter from "../modules/get_project_by_id/app/get_project_by_id_presenter";
+import GetTaskByIdPresenter from "../modules/get_task_by_id/app/get_task_by_id_presenter";
+import GetUserByIdPresenter from "../modules/get_user_by_id/app/get_user_by_id_presenter";
+import UpdateProjectPresenter from "../modules/update_project_by_id/app/update_project_presenter";
 import UpdateProjectStatusPresenter from "../modules/update_project_status/app/update_project_status_presenter";
-
+import UpdateUserStatusPresenter from "../modules/update_user_status/app/update_user_status_presenter";
 
 const routes = (app: Express) => {
   app
@@ -39,6 +39,8 @@ const routes = (app: Express) => {
 
   // task routes
   app.use("/api", CreateTaskPresenter)
+  app.use("/api",GetTaskByIdPresenter)
 };
+
 
 export default routes;
