32ca0b58c7abbdf9916efdee2357158954a20b6b
YuriDrapack <drapackyuri@gmail.com>
2024-07-06 16:43:44 -0300

    refactor: Update GetTaskByIdViewmodel to include optional task properties
    This commit updates the GetTaskByIdViewmodel class to include optional properties for taskDescription, taskFinishDate, and taskCreatedAt. These properties are now assigned from the task object passed to the constructor. This change allows for more flexibility when creating instances of GetTaskByIdViewmodel.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (`refactor` for code refactoring).

---
+++ src/modules/get_task_by_id/app/get_task_by_id_viewmodel.ts
@@ -0,0 +1,24 @@
+import { Task } from "../../../shared/domain/entities/task";
+import { STATUS } from "../../../shared/domain/enums/status_enum";
+
+export class GetTaskByIdViewmodel {
+    taskId?: number;
+    taskName: string;
+    create_user_id: string;
+    project_id: string;
+    taskStatus: STATUS;
+    taskDescription?: string;
+    taskFinishDate?: Date;
+    taskCreatedAt?: Date;
+
+    constructor(task: Task) {
+        this.taskId = task.taskId ?? '';
+        this.taskName = task.taskName;
+        this.create_user_id = task.create_user_id;
+        this.project_id = task.project_id;
+        this.taskStatus = task.taskStatus;
+        this.taskDescription = task.taskDescription;
+        this.taskFinishDate = task.taskFinishDate;
+        this.taskCreatedAt = task.taskCreatedAt;
+    }
+}
