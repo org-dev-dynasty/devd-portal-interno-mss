9bb4793ef5431f2ccde3b8deaa7fd010b1a01749
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-05 22:06:37 -0300

    refactor: Update createUser method in UserRepositoryInterface to accept CreateUserProps

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -1,7 +1,8 @@
+import { CreateUserProps } from "../../../modules/create_user/app/create_user_controller";
 import { IUserAll, User } from "../entities/user";
 
 export interface IUserRepository {
-  createUser(user: User): Promise<User>;
+  createUser(user: CreateUserProps): Promise<User>;
   getUserByEmail(email: string): Promise<IUserAll | undefined>;
   getUserById(id: string): Promise<User | undefined>;
   getAllUsers(): Promise<User[]>;
