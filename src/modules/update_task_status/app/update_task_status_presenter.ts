d2370412e517b3ecf0b161241e975461a8c193a3
YuriDrapack <drapackyuri@gmail.com>
2024-07-06 20:25:12 -0300

    refactor: Add updateTaskStatusController to handle updating task status

---
+++ src/modules/update_task_status/app/update_task_status_presenter.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import { TaskRepositoryPrisma } from "../../../shared/infra/repositories/task_repository_prisma";
+import { updateTaskStatusController } from "./update_task_status_controller";
+import { UpdateTaskStatusUsecase } from "./update_task_status_usecase";
+
+const router = express.Router();
+const taskRepository = new TaskRepositoryPrisma();
+const updateTaskStatusUsecase = new UpdateTaskStatusUsecase(taskRepository);
+const taskController = new updateTaskStatusController(updateTaskStatusUsecase);
+
+router.put("/update-task-status", async (req: Request, res: Response) => {
+  await taskController.handle(req, res);
+});
+
+export default router;
