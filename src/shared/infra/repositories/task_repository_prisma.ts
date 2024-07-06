dfd5c84e137f7a896c2ab3d94610f538331be008
YuriDrapack <drapackyuri@gmail.com>
2024-07-06 20:26:05 -0300

    refactor: Update task_repository_prisma.ts to include updateTask method

--- src/shared/infra/repositories/task_repository_prisma.ts
+++ src/shared/infra/repositories/task_repository_prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { Task, TaskProps } from "../../../shared/domain/entities/task";
-import { STATUS } from "../../domain/enums/status_enum";
+import { TASK_STATUS } from "../../domain/enums/task_status_enum";
 import { ITaskRepository } from "../../domain/repositories/task_repository_interface";
 
 const prisma = new PrismaClient();
@@ -29,7 +29,7 @@ export class TaskRepositoryPrisma implements ITaskRepository {
       });
       const createdTask = new Task({
         taskName: createdTaskFromPrisma.name,
-        taskStatus: createdTaskFromPrisma.status as STATUS,
+        taskStatus: createdTaskFromPrisma.status as TASK_STATUS,
         taskDescription: createdTaskFromPrisma.description,
         taskFinishDate: createdTaskFromPrisma.finish_date,
         taskCreatedAt: createdTaskFromPrisma.created_at,
@@ -59,7 +59,7 @@ export class TaskRepositoryPrisma implements ITaskRepository {
       const task = new Task({
         taskId: taskFromPrisma.task_id,
         taskName: taskFromPrisma.name,
-        taskStatus: taskFromPrisma.status as STATUS,
+        taskStatus: taskFromPrisma.status as TASK_STATUS,
         taskDescription: taskFromPrisma.description,
         taskFinishDate: taskFromPrisma.finish_date,
         taskCreatedAt: taskFromPrisma.created_at,
@@ -73,5 +73,73 @@ export class TaskRepositoryPrisma implements ITaskRepository {
     }
   }
 
+  async updateTask(taskProps: TaskProps): Promise<Task> {
+    try {
+      const data: any = {};
+
+    if (taskProps.taskName !== undefined) {
+      data.name = taskProps.taskName;
+    }
+
+    if (taskProps.taskStatus !== undefined) {
+      data.status = taskProps.taskStatus;
+    }
+
+    if (taskProps.taskDescription !== undefined) {
+      data.description = taskProps.taskDescription;
+    }
+
+    if (taskProps.taskFinishDate !== undefined) {
+      data.finish_date = taskProps.taskFinishDate;
+    }
+
+    if (taskProps.taskCreatedAt !== undefined) {
+      data.created_at = taskProps.taskCreatedAt;
+    }
+
+    if (taskProps.project_id !== undefined) {
+      data.project = {
+        connect: {
+          project_id: taskProps.project_id,
+        },
+      };
+    }
+
+    if (taskProps.create_user_id !== undefined) {
+      data.create_user = {
+        connect: {
+          user_id: taskProps.create_user_id,
+        },
+      };
+    }
+
+    const updatedTaskFromPrisma = await prisma.task.update({
+      where: {
+        task_id: taskProps.taskId,
+      },
+      data,
+    });
+
+
+      const updatedTask = new Task({
+        taskId: updatedTaskFromPrisma.task_id,
+        taskName: updatedTaskFromPrisma.name,
+        taskStatus: updatedTaskFromPrisma.status as TASK_STATUS,
+        taskDescription: updatedTaskFromPrisma.description,
+        taskFinishDate: updatedTaskFromPrisma.finish_date,
+        taskCreatedAt: updatedTaskFromPrisma.created_at,
+        create_user_id: updatedTaskFromPrisma.create_user_id,
+        project_id: updatedTaskFromPrisma.project_id
+      });
+      return updatedTask;
+    } catch (error: any) {
+      console.error("Erro ao atualizar tarefa:", error);
+      throw new Error("Erro ao atualizar tarefa no banco de dados.");
+    }
+  }
+
+
+
 }
 
+ 
