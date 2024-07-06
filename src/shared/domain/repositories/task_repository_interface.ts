9bbb2b99b51c698d91ce75c9cb32577ae8b94cc0
YuriDrapack <drapackyuri@gmail.com>
2024-07-06 20:26:00 -0300

    refactor: Update task_repository_interface to include updateTask method

--- src/shared/domain/repositories/task_repository_interface.ts
+++ src/shared/domain/repositories/task_repository_interface.ts
@@ -2,7 +2,7 @@ import { Task, TaskProps } from "../entities/task";
 
 export interface ITaskRepository {
     createTask(taskProps: TaskProps): Promise<Task>;
-    // updateTask(task: Task): Promise<Task>;
+    updateTask(task: Task): Promise<Task>;
     // deleteTask(taskId: string): Promise<void>;
     getTaskById(taskId: number): Promise<Task | undefined>;
     // getAllTasks(): Promise<Task[]>;
