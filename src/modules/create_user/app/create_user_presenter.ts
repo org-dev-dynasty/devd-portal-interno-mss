b91950b693d470e3c5894fcff0b27009f1df6d21
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-05 22:06:15 -0300

    refactor: Add authentication middleware to create-user route

--- src/modules/create_user/app/create_user_presenter.ts
+++ src/modules/create_user/app/create_user_presenter.ts
@@ -2,13 +2,14 @@ import express, { Request, Response } from "express";
 import { CreateUserController } from "./create_user_controller";
 import { CreateUserUsecase } from "./create_user_usecase";
 import { UserRepositoryPrisma } from "../../../shared/infra/repositories/user_repository_prisma";
+import { authenticateToken } from "../../../shared/middlewares/jwt_middleware";
 
 const router = express.Router();
 const userRepository = new UserRepositoryPrisma(); 
 const createUserUsecase = new CreateUserUsecase(userRepository);
 const userController = new CreateUserController(createUserUsecase);
 
-router.post("/create-user", async (req: Request, res: Response) => {
+router.post("/create-user", authenticateToken, async (req: Request, res: Response) => {
   await userController.createUser(req, res);
 });
 
