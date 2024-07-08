b27be0965ea6257d5051a7c259d2587098468e42
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-08 19:06:44 -0300

    refactor: Update GetTaskByIdController error handling

--- src/modules/get_task_by_id/app/get_task_by_id_controller.ts
+++ src/modules/get_task_by_id/app/get_task_by_id_controller.ts
@@ -1,5 +1,10 @@
-import { GetTaskByIdUseCase } from "./get_task_by_id_usecase";
 import { Request, Response } from "express";
+import { GetTaskByIdUseCase } from "./get_task_by_id_usecase";
+import { GetTaskByIdViewmodel } from "./get_task_by_id_viewmodel";
+import { InvalidParameter, InvalidRequest } from "../../../shared/helpers/errors/controller_errors";
+import { BadRequest, Forbidden, InternalServerError, ParameterError } from "../../../shared/helpers/http/http_codes";
+import { EntityError } from "../../../shared/helpers/errors/domain_errors";
+import { NoItemsFound } from "../../../shared/helpers/errors/usecase_errors";
 
 export class GetTaskByIdController {
     constructor(private getTaskByIdUsecase: GetTaskByIdUseCase) {}
@@ -18,9 +23,22 @@ export class GetTaskByIdController {
 
       return res.status(200).json(taskViewModel);
     } catch (error: any) {
-      console.error("Error in handle:", error);
-      return res.status(500).json({ error: error.message });
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message).send(res);
+      }
+      if (error instanceof InvalidParameter) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof EntityError) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof Forbidden) {
+        return new Forbidden(error.getMessage()).send(res);
+      }
+      if (error instanceof NoItemsFound) {
+        return new Forbidden(error.message).send(res);
+      }
+      return new InternalServerError('Internal Server Error').send(res);
     }
   }
-}
 }
