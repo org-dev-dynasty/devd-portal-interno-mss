8dd2153d79c12ddbfe854aa2e9c4c74f0bba9a50
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-09 04:13:23 -0300

    refactor: Update GetAllTasksByProjectViewmodel to remove unused code and improve readability

--- src/modules/get_all_tasks_by_project/app/get_all_tasks_by_project_viewmodel.ts
+++ src/modules/get_all_tasks_by_project/app/get_all_tasks_by_project_viewmodel.ts
@@ -1,7 +1,7 @@
 import { Task } from "../../../shared/domain/entities/task";
 import { STATUS } from "../../../shared/domain/enums/status_enum";
 
-export class TasksByProjectViewmodel {
+export class GetAllTasksByProjectViewmodel {
   taskId?: number;
   taskName: string;
   create_user_id: string;
@@ -20,35 +20,5 @@ export class TasksByProjectViewmodel {
     this.taskDescription = task.taskDescription;
     this.taskFinishDate = task.taskFinishDate;
     this.taskCreatedAt = task.taskCreatedAt;
-  }
-
-  toJSON() {
-    return {
-      task_id: this.taskId,
-      task_name: this.taskName,
-      create_user_id: this.create_user_id,
-      project_id: this.project_id,
-      task_status: this.taskStatus,
-      task_description: this.taskDescription,
-      task_finish_date: this.taskFinishDate,
-      task_created_at: this.taskCreatedAt,
-    };
-  }
-}
-
-export class GetAllTasksByProjectViewmodel {
-  private tasksViewmodel: TasksByProjectViewmodel[]
-
-  constructor(tasks: Task[]) {
-    this.tasksViewmodel = tasks.map(
-      (task) => new TasksByProjectViewmodel(task)
-    )
-  }
-
-  toJSON() {
-    return {
-      projects: this.tasksViewmodel.map((project) => project.toJSON()),
-      message: 'All projects have been retrieved successfully',
-    }
   }
 }
