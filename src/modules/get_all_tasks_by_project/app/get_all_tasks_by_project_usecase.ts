f6ba9e3e8831b568f27ba18f33119e94d0a3ae27
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-09 17:13:15 -0300

    refactor: Add participant check in GetAllTasksByProjectUsecase

--- src/modules/get_all_tasks_by_project/app/get_all_tasks_by_project_usecase.ts
+++ src/modules/get_all_tasks_by_project/app/get_all_tasks_by_project_usecase.ts
@@ -1,22 +1,24 @@
 import { ITaskRepository } from "../../../shared/domain/repositories/task_repository_interface";
 import { EntityError } from "../../../shared/helpers/errors/domain_errors";
+import { UnprocessableEntity } from "../../../shared/helpers/http/http_codes";
 
 export class GetAllTasksByProjectUsecase {
   constructor(private repo: ITaskRepository) {}
 
   async execute(userId: string, projectId: string) {
-    console.log("PROJECT ID NO USECASE: " + projectId )
     if (!userId) {
       throw new EntityError("user id");
     }
     if (!projectId) {
       throw new EntityError("project id");
     }
-    try {
-      const tasks = this.repo.getAllTasksByProject(userId, projectId);
-      return tasks;
-    } catch (error) {
-      throw new EntityError("Error getting tasks");
+
+    const isParticipant = await this.repo.isUserParticipantOfProject(userId, projectId);
+    if (!isParticipant) {
+      throw new UnprocessableEntity("User not participant of this project.");
     }
+
+    const tasks = await this.repo.getAllTasksByProject(projectId);
+    return tasks;
   }
 }
