e3c57d1d9c5c659dec472c9f3b11e8583eadbb76
YuriDrapack <drapackyuri@gmail.com>
2024-07-12 02:30:18 -0300

    refactor: Add input validation to GetTaskByIdController

--- src/modules/get_task_by_id/app/get_task_by_id_controller.ts
+++ src/modules/get_task_by_id/app/get_task_by_id_controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { GetTaskByIdUseCase } from "./get_task_by_id_usecase";
-import { GetTaskByIdViewmodel } from "./get_task_by_id_viewmodel";
-import { InvalidParameter, InvalidRequest } from "../../../shared/helpers/errors/controller_errors";
-import { BadRequest, Forbidden, InternalServerError, ParameterError } from "../../../shared/helpers/http/http_codes";
+import { InvalidParameter, InvalidRequest, MissingParameters } from "../../../shared/helpers/errors/controller_errors";
 import { EntityError } from "../../../shared/helpers/errors/domain_errors";
 import { NoItemsFound } from "../../../shared/helpers/errors/usecase_errors";
+import { BadRequest, Forbidden, InternalServerError, ParameterError, UnprocessableEntity } from "../../../shared/helpers/http/http_codes";
+import { GetTaskByIdUseCase } from "./get_task_by_id_usecase";
+import { GetTaskByIdViewmodel } from "./get_task_by_id_viewmodel";
 
 export class GetTaskByIdController {
     constructor(private getTaskByIdUsecase: GetTaskByIdUseCase) {}
@@ -12,6 +12,10 @@ export class GetTaskByIdController {
   async handle(req: Request, res: Response) {
     try {
       const taskId: number = Number(req.params.taskId);
+
+      if (!taskId) {
+        throw new MissingParameters("Invalid task id");
+      }
 
       const task = await this.getTaskByIdUsecase.execute(taskId);
 
@@ -37,6 +41,12 @@ export class GetTaskByIdController {
       }
       if (error instanceof NoItemsFound) {
         return new Forbidden(error.message).send(res);
+      }
+      if (error instanceof UnprocessableEntity) {
+        return new UnprocessableEntity(error.getMessage()).send(res);
+      }
+      if (error instanceof MissingParameters) {
+        return new ParameterError(error.message).send(res);
       }
       return new InternalServerError('Internal Server Error').send(res);
     }
