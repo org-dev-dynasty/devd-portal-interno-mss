581760e38e12ba17e4987937c61a945fad1c0781
YuriDrapack <drapackyuri@gmail.com>
2024-07-17 23:40:52 -0300

    adding update-task repo prisma

--- src/shared/domain/repositories/task_repository_interface.ts
+++ src/shared/domain/repositories/task_repository_interface.ts
@@ -3,7 +3,8 @@ import { Task, TaskProps } from "../entities/task";
 export interface ITaskRepository {
   createTask(taskProps: TaskProps): Promise<Task>;
   deleteTask(taskId: number): Promise<void>;
-  updateTask(task: Task): Promise<Task>;
+  updateTask(id: number, data: Partial<Task>): Promise<Task>;
+  updateTaskStatus(status: string, taskId: number): Promise<Task>;
   getTaskById(taskId: number): Promise<Task | undefined>;
   isUserParticipantOfProject(userId: string, projectId: string): Promise<boolean>;
   getAllTasksByProject(projectId: string): Promise<Task[]>;
