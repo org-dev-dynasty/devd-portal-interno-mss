a6e6df192c9faa540eb47afba92fc7e1208d8ad4
jotaapee <galhardojoaopedro@gmail.com>
2024-07-19 13:09:56 -0300

    refac: Remove authentication middleware from /auth-user route

--- src/modules/auth_user/app/auth_user_presenter.ts
+++ src/modules/auth_user/app/auth_user_presenter.ts
@@ -2,14 +2,13 @@ import express, { Request, Response } from "express";
 import { AuthUserController } from "./auth_user_controller";
 import { AuthUserUsecase } from "./auth_user_usecase";
 import { UserRepositoryPrisma } from "../../../shared/infra/repositories/user_repository_prisma";
-import { authenticateToken } from "../../../shared/middlewares/jwt_middleware";
 
 const router = express.Router();
 const userRepository = new UserRepositoryPrisma();
 const authUserUsecase = new AuthUserUsecase(userRepository);
 const authUserController = new AuthUserController(authUserUsecase);
 
-router.post("/auth-user", authenticateToken, async (req: Request, res: Response) => {
+router.post("/auth-user", async (req: Request, res: Response) => {
   await authUserController.handle(req, res);
 });
 
