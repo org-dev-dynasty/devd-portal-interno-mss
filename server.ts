7b9070b7557b19033758cc0203b9b730459c9441
YuriDrapack <drapackyuri@gmail.com>
2024-07-19 17:19:43 -0300

    refac: Update server.ts to use port 3030 instead of 3020

--- server.ts
+++ server.ts
@@ -1,16 +1,15 @@
 import "dotenv/config";
 import app from "./src/app";
-import routes from "./src/routes/routes";
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3020;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT} 🚀`);
 });
 
 import cors from "cors";
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
