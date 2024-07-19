9b84fe3b33a4e2966743eaaecaad299cdfede108
jotaapee <galhardojoaopedro@gmail.com>
2024-07-19 13:10:07 -0300

    refac: Update CreateProjectController to use Project entity instead of ProjectProps

--- src/modules/create_project/app/create_project_controller.ts
+++ src/modules/create_project/app/create_project_controller.ts
@@ -1,6 +1,6 @@
 // Conteúdo do arquivo...
 import { Request, Response } from "express";
-import { ProjectProps } from "../../../shared/domain/entities/project";
+import { Project } from "../../../shared/domain/entities/project";
 import { CreateProjectViewModel } from "../../../modules/create_project/app/create_project_viewmodel";
 import { CreateProjectUsecase } from "./create_project_usecase";
 import { ParameterError, BadRequest, Forbidden, InternalServerError } from "../../../shared/helpers/http/http_codes";
@@ -12,43 +12,40 @@ import {
 import { EntityError } from "../../../shared/helpers/errors/domain_errors";
 
 export class CreateProjectController {
-  constructor(private usecase: CreateProjectUsecase) {}
+  constructor(private usecase: CreateProjectUsecase) { }
 
   async createProject(req: Request, res: Response) {
     try {
       const userAccess = req.user?.access;
 
-      if(!userAccess?.includes('BTN-CREATE-USER')) {
+      if (!userAccess?.includes('BTN-CREATE-PROJECT')) {
+        throw new Forbidden('You do not have permission to access this feature');
+      }
+
+      const userId = req.user?.user_id;
+      if (!userId) {
         throw new Forbidden('You do not have permission to access this feature');
       }
 
       console.log("TENTANDO CRIAR PROJETO");
       const { projectName, projectStatus, projectDescription } = req.body;
 
-      const errors = [];
 
       if (!projectName) {
-        errors.push(new MissingParameters("Project Name"));
+        throw new MissingParameters("Project Name");
       }
 
       if (!projectStatus) {
-        errors.push(new MissingParameters("Project Status"));
+        throw new MissingParameters("Project Status");
       }
 
       if (!projectDescription) {
-        errors.push(new MissingParameters("Project Description"));
+        throw new MissingParameters("Project Description");
       }
 
-      if (errors.length > 0) {
-        return res.status(400).json(errors);
-      }
+      const project = new Project(projectName, projectDescription, projectStatus);
 
-      const projectProps: ProjectProps = {
-        projectName,
-        projectStatus,
-        projectDescription,
-      };
-      await this.usecase.execute(projectProps);
+      await this.usecase.execute(project);
       const viewmodel = new CreateProjectViewModel(
         "Project created successfully"
       );
